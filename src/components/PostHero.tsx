0219a029fcad0517151e8d67a13e334fbba35ef8
taroj1205 <taroj1205@gmail.com>
2023-10-18 19:06:22 +1300

    remove margin left

--- src/components/PostHero.tsx
+++ src/components/PostHero.tsx
@@ -71,7 +71,7 @@ export default function PostHero() {
                                             <path d='M12 5l7 7-7 7'></path>
                                         </svg>
                                     </Link>
-                                    <div className='md:ml-2 order-1 md:order-2 flex items-center'>
+                                    <div className='order-1 md:order-2 flex items-center'>
                                         <span className='text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200 dark:border-gray-500'>
                                             {readTime}
                                         </span>
