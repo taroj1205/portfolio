7b40e5af44f6d33cc34b5f48fdc3d16c563d79c7
taroj1205 <taroj1205@gmail.com>
2023-10-18 14:06:20 +1300

    Update PostCard.tsx

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -30,10 +30,10 @@ export default function ArticleCard({ image, category, title, description, readi
                                         {title}
                                     </h1>
                                     <p className='leading-relaxed mb-3 text-gray-600 dark:text-gray-300 line-clamp-2'>{description}</p>
-                                    <div className='flex items-center flex-wrap '>
+                                    <div className='flex items-center flex-wrap justify-between'>
                                         <Link
                                             href={`/posts/${slug}`}
-                                            className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0'
+                                            className='text-indigo-500 inline-flex items-center'
                                         >
                                             {locale === 'ja' ? 'もっと読む' : 'Read More'}
                                             <svg
@@ -49,7 +49,7 @@ export default function ArticleCard({ image, category, title, description, readi
                                                 <path d='M12 5l7 7-7 7'></path>
                                             </svg>
                                         </Link>
-                                        <div className='ml-2'>
+                                        <div className='ml-2 flex items-center'>
                                             <span className='text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200'>
                                                 {readTime}
                                             </span>
