f3e4a79db8513a3013b6c007120df9b57e49bf40
taroj1205 <taroj1205@gmail.com>
2023-10-22 23:09:24 +1300

    Update NextHeader.tsx

--- src/components/NextHeader.tsx
+++ src/components/NextHeader.tsx
@@ -111,6 +111,7 @@ export default function NextHeader() {
             }}
             shouldHideOnScroll
             onMenuOpenChange={setIsMenuOpen}
+            isBordered
         >
             <NavbarContent>
                 <NavbarMenuToggle
