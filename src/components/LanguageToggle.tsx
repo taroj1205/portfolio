5b7ac9902cfa4f23374c3dcc4a4ace3eed93b00c
taroj1205 <taroj1205@gmail.com>
2023-11-07 19:54:24 +1300

    slightly improve performance

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -1,6 +1,12 @@
-'use client'
+"use client";
 import React from "react";
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@nextui-org/react";
+import {
+	Dropdown,
+	DropdownTrigger,
+	DropdownMenu,
+	DropdownItem,
+	Avatar,
+} from "@nextui-org/react";
 import { usePathname, useRouter } from "@/lib/next-intl";
 import { useLocale } from "next-intl";
 import Image from "next/image";
@@ -9,81 +15,95 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import nzFlag from "@/../public/svg/flag/nz.svg";
 import jpFlag from "@/../public/svg/flag/jp.svg";
+import { FaSpinner } from "react-icons/fa";
 
 const LanguageToggle: React.FC = () => {
-    const router = useRouter();
-    const pathname = usePathname();
-    const currentLocale = useLocale();
-    const params = useSearchParams();
+	const router = useRouter();
+	const pathname = usePathname();
+	const currentLocale = useLocale();
+	const params = useSearchParams();
 
-    const [mounted, setMounted] = useState(false);
+	const [mounted, setMounted] = useState(false);
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
-    if (!mounted) {
-        return null
-    }
+	if (!mounted) {
+		return <FaSpinner className="animate-spin text-gray-500 w-4 h-4" />;
+	}
 
-    // const handleLanguageChange = (locale: string) => {
-    //     console.log(locale, params, params.toString().length, pathname);
-    //     if (params.toString().length > 0) {
-    //         router.push(`${pathname}${params ? `?${params.toString()}` : ''}`, { locale: locale });
-    //         return;
-    //     }
-    //     router.push(`${pathname}`, { locale: locale });
-    // };
+	// const handleLanguageChange = (locale: string) => {
+	//     console.log(locale, params, params.toString().length, pathname);
+	//     if (params.toString().length > 0) {
+	//         router.push(`${pathname}${params ? `?${params.toString()}` : ''}`, { locale: locale });
+	//         return;
+	//     }
+	//     router.push(`${pathname}`, { locale: locale });
+	// };
 
-    return (
-        <Dropdown placement="bottom-end">
-            <DropdownTrigger>
-                {currentLocale === 'ja' ? (
-                    <Avatar
-                        as="button"
-                        title="日本語"
-                        className="transition-transform h-6 w-6"
-                        src="/svg/flag/jp.svg"
-                    />
-                ) : (
-                    <Avatar
-                        as="button"
-                        title="English"
-                        className="transition-transform h-6 w-6"
-                        src="/svg/flag/nz.svg"
-                    />
-                )}
-            </DropdownTrigger>
-            <DropdownMenu selectionMode="single" selectedKeys={[currentLocale]} aria-label="Language Options">
-                <DropdownItem key="en" as={Link} href={`/en${pathname}`} lang="en" className="text-black dark:text-white">
-                    <div className="flex flex-row items-center text-black dark:text-white">
-                        <Image
-                            className="transition-transform mr-1"
-                            src={nzFlag}
-                            height={30}
-                            width={30}
-                            alt="New Zealand flag"
-                            priority
-                        />
-                        English
-                    </div>
-                </DropdownItem>
-                <DropdownItem key="ja" as={Link} href={`/ja${pathname}`} lang="ja" className="flex flex-row items-center text-white">
-                    <div className="flex flex-row items-center text-black dark:text-white">
-                        <Image
-                            className="transition-transform mr-1"
-                            src={jpFlag}
-                            height={30}
-                            width={30}
-                            alt="Japanese flag"
-                            priority
-                        />
-                        日本語
-                    </div>
-                </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
-    );
+	return (
+		<Dropdown placement="bottom-end">
+			<DropdownTrigger>
+				{currentLocale === "ja" ? (
+					<Avatar
+						as="button"
+						title="日本語"
+						className="transition-transform h-6 w-6"
+						src="/svg/flag/jp.svg"
+					/>
+				) : (
+					<Avatar
+						as="button"
+						title="English"
+						className="transition-transform h-6 w-6"
+						src="/svg/flag/nz.svg"
+					/>
+				)}
+			</DropdownTrigger>
+			<DropdownMenu
+				selectionMode="single"
+				selectedKeys={[currentLocale]}
+				aria-label="Language Options">
+				<DropdownItem
+					key="en"
+					as={Link}
+					href={`/en${pathname}`}
+					lang="en"
+					className="text-black dark:text-white">
+					<div className="flex flex-row items-center text-black dark:text-white">
+						<Image
+							className="transition-transform mr-1"
+							src={nzFlag}
+							height={30}
+							width={30}
+							alt="New Zealand flag"
+							priority
+						/>
+						English
+					</div>
+				</DropdownItem>
+				<DropdownItem
+					key="ja"
+					as={Link}
+					href={`/ja${pathname}`}
+					lang="ja"
+					className="flex flex-row items-center text-white">
+					<div className="flex flex-row items-center text-black dark:text-white">
+						<Image
+							className="transition-transform mr-1"
+							src={jpFlag}
+							height={30}
+							width={30}
+							alt="Japanese flag"
+							priority
+						/>
+						日本語
+					</div>
+				</DropdownItem>
+			</DropdownMenu>
+		</Dropdown>
+	);
 };
 
 export default LanguageToggle;
